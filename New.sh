396e2b10d9027481363e6ba27ff1e9999c858278
Bilel3 <30528888+Bilel3@users.noreply.github.com>
2024-03-18 10:48:27 +0100

    Create New.sh

---
+++ New.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Remote host details
+remote_host="username@remote_host"
+remote_path="/path/to/remote/directory"
+
+# Local folder to copy
+local_folder="/path/to/local/folder"
+
+# Check if SSH key exists, if not, create one
+if [ ! -f ~/.ssh/id_rsa ]; then
+    ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa
+fi
+
+# Check if passwordless SSH access is set up
+if ! ssh -o "BatchMode=yes" "$remote_host" true; then
+    echo "First time connecting to remote host. Enter SSH password:"
+    read -s ssh_password
+    # Copy SSH key to remote host
+    sshpass -p "$ssh_password" ssh-copy-id -i ~/.ssh/id_rsa.pub "$remote_host"
+fi
+
+# Copy folders listed in directories.txt to remote host
+while IFS= read -r folder; do
+    scp -r "$local_folder/$folder" "$remote_host":"$remote_path"
+done < directories.txt
+
+# Exclude files matching regex pattern in .exclude file
+exclude_pattern=$(<.exclude)
+rsync -av --exclude="$exclude_pattern" "$local_folder/" "$remote_host":"$remote_path"
